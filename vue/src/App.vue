792eef355ed05a86b5f1112cfeaa048158c63ca1
h56983577 <h56983577@126.com>
2020-08-06 22:59:46 +0800

    Initialize the vue project

---
+++ vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>2020暑假小学期前端项目</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
