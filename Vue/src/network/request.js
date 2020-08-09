e0e866c97ac7e681ef4f0772643b8e59cdfa4d5b
Ruikai Li <58371402+MelonLee-go@users.noreply.github.com>
2020-08-09 23:10:48 +0800

    Hzy utils network (#8)
    * Add new git ignore file to root directory
    
    * Add a network utility package
    
    * Fix a bug in the usage demo
    
    Co-authored-by: h56983577 <h56983577@126.com>

---
+++ Vue/src/network/request.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+/**
+ * when use it in outer module
+ * (maybe you can add a layer to manage url)
+ * ```javascript
+ * import {request} from 'network/request.js'
+ * request({
+ *   url: 8036
+ * }).then(res => {
+ *   // deal with response
+ * }).catch(err => {
+ *   // deal with exception
+ * })
+ * ```
+ * @param config configure object
+ * @returns {AxiosPromise}
+ */
+export function request(config) {
+  const instance = axios.create({
+    baseURL: '',
+    timeout: 5000
+  })
+  instance.interceptors.request.use(config => {
+    return config
+  }, error => {
+    console.log(error)
+  })
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, error => {
+    console.log(error)
+  })
+  return instance(config)
+}
