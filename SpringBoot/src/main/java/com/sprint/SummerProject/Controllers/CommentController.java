c48004ecc6a444e12a5fb7dc883580d88709eaed
LiShushi <15510269739@163.com>
2020-08-10 17:39:27 +0800

    Add CommentController

---
+++ SpringBoot/src/main/java/com/sprint/SummerProject/Controllers/CommentController.java
@@ -0,0 +1,24 @@
+package com.sprint.SummerProject.Controllers;
+
+import com.sprint.SummerProject.Models.Comment;
+import com.sprint.SummerProject.Repositories.CommentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private CommentRepository commentRepository;
+
+    public CommentController(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Comment> getAll(){
+        return commentRepository.findAll();
+    }
+}
