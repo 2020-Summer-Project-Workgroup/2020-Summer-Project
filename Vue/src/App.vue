c4b1d2e731926529118a3298d7a803c27cded984
h56983577 <h56983577@126.com>
2020-08-10 14:41:33 +0800

    Insert TabBar into the main application

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
+    <TabBar/>
     <Welcome/>
   </div>
 </template>
 
 <script>
 import Welcome from "@/views/welcome/Welcome";
+import TabBar from "@/component/tarBar/TabBar";
 export default {
   name: 'App',
   components: {
+    TabBar,
     Welcome
   }
 }
 </script>
 
 <style>
-body {
-  font-size: 15px;
+html body {
+  font-size: 12px;
   margin: 0;
   padding: 0;
 }
