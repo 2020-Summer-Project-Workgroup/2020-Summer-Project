c8330a321b40cc3bd9269a5b92db6cd82d88d595
h56983577 <h56983577@126.com>
2020-08-10 19:54:11 +0800

    Add a plain register page.

---
+++ Vue/src/views/register/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="register-container">
+    <div class="title">
+      <span>注册</span>
+    </div>
+    <div class="register-form">
+      <input type="text" placeholder="username"/>
+      <input type="text" placeholder="验证码"/>
+      <input type="password" placeholder="password"/>
+      <button @click="submit">登录</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  methods: {
+    submit() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
