d522ada9a9217c782900d29b035682e15ca4a8ac
h56983577 <49408466+h56983577@users.noreply.github.com>
2020-08-10 15:42:34 +0800

    Hzy feature tab bar item 1.0 (#13)
    * Add TabBarItem component
    
    * Add TabBar component
    
    * Insert TabBar into the main application
    
    * Adjust css styles of the TabBar
    
    * Delete the nonsense configure file

---
+++ Vue/src/component/tarBar/TabBar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="main-bar">
+    <div class="logo">
+      <span>Casual doc</span>
+    </div>
+    <div class="login-bar">
+      <TabBarItem link="/login">
+        <span>登录</span>
+      </TabBarItem>
+      <span class="split">|</span>
+      <TabBarItem link="/register">
+        <span>注册</span>
+      </TabBarItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import TabBarItem from "@/component/tarBar/TabBarItem";
+export default {
+  name: "TabBar",
+  components: {TabBarItem}
+}
+</script>
+
+<style scoped>
+.main-bar {
+  height: 5rem;
+  width: 100vw;
+  background-color: #ccc;
+  box-shadow: 0 2px 1px rgba(255, 255, 255, .15);
+  top: 0;
+  position: absolute;
+}
+
+.main-bar .logo {
+  height: 5rem;
+  width: 25vw;
+  font-size: 1.5rem;
+  top: 0;
+  left: 15vw;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.main-bar .logo span{
+  height: 5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.main-bar .login-bar {
+  height: 5rem;
+  width: 45vw;
+  top: 0;
+  right: 15vw;
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.split {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-size: 20px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+}
+</style>
