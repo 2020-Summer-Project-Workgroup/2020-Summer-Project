9649aad6ab0c8cbdc60d457645c9d654f20fa600
h56983577 <h56983577@126.com>
2020-08-11 16:12:27 +0800

    Add a new class to generate verify code.

---
+++ SpringBoot/src/test/java/com/sprint/SummerProject/util/CodeGeneratorTest.java
@@ -0,0 +1,11 @@
+package com.sprint.SummerProject.util;
+
+import org.junit.jupiter.api.Test;
+
+public class CodeGeneratorTest {
+
+    @Test
+    public void test() {
+        System.out.println(CodeGenerator.generateCode());
+    }
+}
