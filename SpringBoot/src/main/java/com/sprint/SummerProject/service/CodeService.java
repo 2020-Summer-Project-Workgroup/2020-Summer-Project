974bff1485075a6d577f2965e56e714f6e810a24
h56983577 <h56983577@126.com>
2020-08-11 15:50:24 +0800

    Add Code Service.

---
+++ SpringBoot/src/main/java/com/sprint/SummerProject/service/CodeService.java
@@ -0,0 +1,46 @@
+package com.sprint.SummerProject.service;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+/*
+pom.xml
+<dependency>
+  <groupId>com.aliyun</groupId>
+  <artifactId>aliyun-java-sdk-core</artifactId>
+  <version>4.5.3</version>
+</dependency>
+*/
+public class CodeService {
+
+    public void sendCode(String tel) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        request.putQueryParameter("PhoneNumbers", "15911103365");
+        request.putQueryParameter("SignName", "轻松文档");
+        request.putQueryParameter("TemplateCode", "SMS_199222535");
+        request.putQueryParameter("TemplateParam", "{\"code\":\"111111\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
