cfdbadf0252979b39f8951f9690775c89d6fad5b
h56983577 <49408466+h56983577@users.noreply.github.com>
2020-08-12 19:19:06 +0800

    Login Page 1.2 (#31)
    * Test commit.
    
    * Complete logic and css style of the login page.

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <TabBar/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import TabBar from "@/component/tarBar/TabBar";
 export default {
   name: 'App',
   components: {
-    TabBar
   }
 }
 </script>
--- Vue/src/views/login/Login.vue
+++ Vue/src/views/login/Login.vue
@@ -4,9 +4,30 @@
       <span>登录</span>
     </div>
     <div class="login-form">
-      <input type="text" placeholder="username" v-model="username"/>
-      <input type="password" placeholder="password" v-model="password"/>
-      <button @click="submit">登录</button>
+      <div class="form-item">
+        <div v-if="isTel">
+          <label for="tel" style="top: 11.14%">
+            <span :style="telColor" @click="switchToTel">输入手机</span>
+            /
+            <span :style="emailColor" @click="switchToEmail">邮箱</span>
+          </label>
+          <input type="text" id="tel" name="tel" v-model="tel" style="top: 19.03%"/>
+        </div>
+        <div v-else>
+          <label for="email" style="top: 11.14%">
+            <span :style="telColor" @click="switchToTel">输入手机</span>
+            /
+            <span :style="emailColor" @click="switchToEmail">邮箱</span>
+          </label>
+          <input type="text" id="email" name="username" v-model="email" style="top: 19.03%"/>
+        </div>
+      </div>
+      <div class="form-item">
+        <label for="password" style="top: 35.83%">输入密码</label>
+        <input type="password" id="password" name="password" v-model="password" style="top: 44.08%"/>
+      </div>
+      <button id="login" @click="submit">登录CasualDoc</button>
+      <span class="text-link" id="to-register" @click="toRegister">新用户注册</span>
     </div>
   </div>
 </template>
@@ -18,17 +39,52 @@ export default {
   name: "Login",
   data() {
     return {
-      username: '',
-      password: ''
+      isTel: true,
+      tel: '',
+      email: '',
+      password: '',
     }
   },
   methods: {
+    switchToTel() {
+      this.isTel = true
+      this.reset()
+    },
+    switchToEmail() {
+      this.isTel = false
+      this.reset()
+    },
+    reset() {
+      this.tel = ''
+      this.email = ''
+      this.password = ''
+    },
+    toRegister() {
+      this.$router.push('/register');
+    },
+    // 网络请求方法
     submit() {
       checkInformation(this.username, this.password).then(res => {
         console.log(res)
       }).catch(err => {
         console.log(err)
       })
+    },
+  },
+  computed: {
+    telColor() {
+      if (this.isTel) {
+        return {color: '#606060'}
+      } else {
+        return {color: '#CFCFCF'}
+      }
+    },
+    emailColor() {
+      if (this.isTel) {
+        return {color: '#CFCFCF'}
+      } else {
+        return {color: '#606060'}
+      }
     }
   }
 }
@@ -40,8 +96,8 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  position: fixed;
-  background-image: linear-gradient(to right, #A2A8B8, #CED6EB);
+  position: absolute;
+  background-color: rgba(242, 244, 244, 1);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -50,22 +106,115 @@ export default {
 }
 
 .title {
-  font-size: 2rem;
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  padding: 20px;
+  position: relative;
+  font-family: "Hiragino Sans GB", "Helvetica Neue", Helvetica,
+  "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 41px;
+  line-height: 54px;
+  color: #616161;
 }
 
 .login-form {
-  height: 40vh;
-  width: 30vw;
-  padding: 20px;
-  border-radius: 16px;
+  width: 32.81vw;
+  height: 55.43vh;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  background: #FFFFFF;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.05);
   border: none;
-  background-color: #ccc;
-  box-shadow: -8px -8px 4px rgb(255, 255, 255),
-              8px 8px 4px rgba(0, 0, 0, .15);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  border-radius: 10px;
+}
+
+input {
+  width: 70.55%;
+  height: 11.65%;
+  left: 15.28%;
+  position: absolute;
+  font-size: 20px;
+  font-family: "Hiragino Sans GB", "Helvetica Neue", Helvetica,
+  "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  background: #ffffff;
+  border: 1px solid #cfcfcf;
+  border-radius: 5px;
+  box-shadow: inset 0 4px 4px rgba(0, 0, 0, 0.05), 0 4px 4px rgba(0, 0, 0, 0.05);
+}
+
+input:hover {
+  border: 1px solid #54A293;
+  background-color: #F2F4F4;
+}
+
+input:focus {
+  outline: none;
+  border: 1px solid #54A293;
+  background-color: #F2F4F4;
+}
+
+label {
+  left: 15.28%;
+  position: absolute;
+  font-family: "Hiragino Sans GB", "Helvetica Neue", Helvetica,
+  "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+  color: #606060;
+}
+
+button {
+  position: absolute;
+  border: none;
+  border-radius: 5px;
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.2) 0%,
+  rgba(255, 255, 255, 0) 100%), #54A293;
+  font-family: "Hiragino Sans GB", "Helvetica Neue", Helvetica,
+  "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  color: #FFFFFF;
+}
+
+button:hover {
+  background-color: #368375;
+}
+
+button:active {
+  outline: none;
+  background-color: #368375;
+  color: #FFF492;
+}
+
+#login {
+  width: 70.55%;
+  left: 15.28%;
+  top: 66.02%;
+  bottom: 22.33%;
+  font-size: 25px;
+  line-height: 33px;
+  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.25);
+}
+
+.text-link {
+  left: calc(50% - 50px);
+  top: 85.63%;
+  bottom: 8.75%;
+  position: absolute;
+  font-family: "Hiragino Sans GB", "Helvetica Neue", Helvetica,
+  "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 26px;
+  color: #62AAFF;
+}
+
+.text-link:hover {
+  cursor: pointer;
+  text-decoration: underline;
 }
 </style>
