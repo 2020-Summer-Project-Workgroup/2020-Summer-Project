ac508b18d066d3f04a77da9e4dae2aade57ef89d
h56983577 <49408466+h56983577@users.noreply.github.com>
2020-08-14 02:24:44 +0800

    Update main.js
    Restore a deleted line, which maybe important to the project.

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -4,6 +4,9 @@ import router from "router";
 import store from "@/store";
 import ElementUI from 'element-ui' //element-ui的全部组件
 import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+
+Vue.config.productionTip = false
+
 Vue.use(ElementUI) //使用elementUI
 
 new Vue({
