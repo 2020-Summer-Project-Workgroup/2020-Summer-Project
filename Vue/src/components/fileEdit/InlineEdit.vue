32ea54f3f316874bfd09ae7e95530d2ee0e96191
LSS <15510269739@163.com>
2020-08-14 17:16:05 +0800

    Change code style to correct format

--- Vue/src/components/fileEdit/InlineEdit.vue
+++ Vue/src/components/fileEdit/InlineEdit.vue
@@ -1,31 +1,30 @@
 <template>
-    <div id="app">
-        <ckeditor type="inline" v-model="editorData" :config="editorConfig" ></ckeditor>
-    </div>
+	<div id="app">
+		<ckeditor type="inline" v-model="editorData" :config="editorConfig"></ckeditor>
+	</div>
 </template>
 
-<script src="../node_modules/ckeditor4/ckeditor.js"></script>
-<script src="../node_modules/ckeditor4-vue/dist/ckeditor.js"></script>
-<script >
-		import Vue from 'vue';
-		import CKEditor from 'ckeditor4-vue';
-		Vue.use( CKEditor );
-		
-    export default {
-        name: 'app',
-        components: {
-            // Use the <ckeditor> component in this view.
-            ckeditor: CKEditor.component
-        },
-        data() {	
-            return {
-                editorData: '<p>Content of the editor.</p>',
-                editorConfig: {
-                    // The configuration of the editor.
-                }
-            };
-        }
-    }
+<script>
+import Vue from 'vue'
+import CKEditor from 'ckeditor4-vue'
+
+Vue.use(CKEditor);
+
+export default {
+	name: 'app',
+	components: {
+		// Use the <ckeditor> component in this view.
+		ckeditor: CKEditor.component
+	},
+	data() {
+		return {
+			editorData: '<p>Content of the editor.</p>',
+			editorConfig: {
+				// The configuration of the editor.
+			}
+		};
+	}
+}
 </script>
 
 <style>
