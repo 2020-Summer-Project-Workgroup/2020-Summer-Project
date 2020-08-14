495d2205d659eb584b1fc7d4292ec3f40e9e3aac
h56983577 <h56983577@126.com>
2020-08-14 08:00:31 +0800

    Complete the network request feature of login page.

--- Vue/src/network/login.js
+++ Vue/src/network/login.js
@@ -1,11 +1,22 @@
 import {request} from "@/network/request";
 
-export function checkInformation(username, password) {
+export function loginByTel(tel, password) {
   return request({
-    url: '/user',
+    url: '/session/tel',
     method: 'GET',
     params: {
-      username: username,
+      tel: tel,
+      password: password
+    }
+  })
+}
+
+export function loginByEmail(email, password) {
+  return request({
+    url: '/session/email',
+    method: 'GET',
+    params: {
+      email: email,
       password: password
     }
   })
