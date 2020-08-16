7bbef678267c95b8107ca481afab0367a535fb43
LSS <15510269739@163.com>
2020-08-16 16:09:18 +0800

    Fix icon not show problem in RightSideBar and add elementUI import define in main.js

--- Vue/src/components/rightSideBar/RightSideBar.vue
+++ Vue/src/components/rightSideBar/RightSideBar.vue
@@ -1,37 +1,28 @@
 <template>
   <div id="RBar">
-    <div class="button greenButton" id="newFile" @mouseenter="me1" @mouseleave="ml1" @click="cl1" :style="newStyle">
-      <!--待加入跳转链接-->
-      <span class="text">
-        新建
-      </span>
+    <div class="button greenButton" id="newFile" @click="cl1" :style="newStyle">
+      <span class="text">新建</span>
     </div>
     <br />
-    <div class="button stdButton" id="From" @mouseenter="me2" @mouseleave="ml2" @click="cl2" :style="newStyle">
-      <span class="text">
-        从模板新建
-      </span>
+    <div class="button stdButton" id="from" @mouseenter="me2" @mouseleave="ml2" @click="cl2" :style="newStyle">
+      <span class="text">从模板新建</span>
     </div>
     <br />
     <div id="down" class="StdSecond">
-      <span class="stdFont">
-        最近浏览
-      </span>
+      <span class="stdFont">最近浏览</span>
       <span id="detail">
-        <a href="" /> 详情
+        <a href /> 详情
       </span>
-
-      <!--
-      <br />
-      <hr id="line" color="#cfcfcf" size="1px" width="95%" />
-      <br />
-      -->
     </div>
     <div class="ReftBarBottom">
       <table>
         <tbody>
           <tr v-for="file in files" :key="file.name" class="stdFont">
-            <img :src="'../../assets/' + file.type + '.png'" /> {{ file.name }} <i class="el-icon-s-tools"></i>
+            <img src="../../assets/个人.png" v-if="file.type === 1" />
+            <img src="../../assets/团队.png" v-else-if="file.type === 2" />
+            <img src="../../assets/收藏.png" v-else-if="file.type === 3" />
+            {{ file.name }}
+            <i class="el-icon-s-tools"></i>
           </tr>
         </tbody>
       </table>
@@ -40,31 +31,31 @@
 </template>
 
 <script>
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
 export default {
+  components: {
+
+  },
   data: function() {
     return {
+      iconPath: "../../assets/个人.png",
       files: [{
-          type: "个人",
-          name: "示例文档"
+          type: 1,  //1 代表个人文档
+          name: "示例文档",
         },
         {
-          type: "团队",
-          name: "LSS"
+          type: 2,  //2 代表团队文档
+          name: "LSS",
         },
         {
-          type: "收藏",
-          name: "qwertyuiasdfghjkzxvcbnm"
+          type: 3,  //3 代表收藏文档
+          name: "qwertyuiasdfghjkzxvcbnm",
         },
       ],
-    }
+    };
   },
-  methods: {
-  }
-}
+  
+  methods: {},
+};
 </script>
 
 <style scoped>
@@ -95,7 +86,8 @@ export default {
   height: 48px;
   left: 49px;
   top: 123px;
-  background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%), #E5E5E5;
+  background: linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0) 100%),
+    #e5e5e5;
   text-align: center;
 }
 
@@ -105,7 +97,10 @@ export default {
   height: 48px;
   left: 49px;
   top: 62px;
-  background: linear-gradient(180deg, rgba(255, 255, 255, 0.2) 0%, rgba(255, 255, 255, 0) 100%), #54A293;
+  background: linear-gradient(180deg,
+    rgba(255, 255, 255, 0.2) 0%,
+    rgba(255, 255, 255, 0) 100%),
+    #54a293;
 }
 
 .text {
@@ -114,16 +109,17 @@ export default {
 }
 
 .stdFont {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 20px;
   color: rgba(97, 97, 97, 1);
-
 }
 
 #newFile {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 20px;
@@ -132,7 +128,8 @@ export default {
 }
 
 #RBar {
-  position: absolute;
+  display: -webkit-flex;
+  display: flex;
   width: 19.7vw;
   height: calc(100vh - 68px);
   right: 0;
@@ -143,8 +140,9 @@ export default {
   border-width: 1px;
 }
 
-#From {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+#from {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 20px;
@@ -155,11 +153,12 @@ export default {
 }
 
 #detail {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 16px;
-  color: #54A293;
+  color: #54a293;
 }
 
 #setBut {
