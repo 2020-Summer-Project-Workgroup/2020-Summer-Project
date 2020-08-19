d58531259140bbffe132122eefe98fe50cfb946a
h56983577 <h56983577@126.com>
2020-08-19 22:44:47 +0800

    Modify the file list.

--- Vue/src/store/modules/files.js
+++ Vue/src/store/modules/files.js
@@ -1,6 +1,17 @@
 export default {
   state: {
-    files: []
+    files: [
+      {
+        id: "5f3d23777086051e66f7a1b2",
+        title: "LOL",
+        content:"Hello World",
+        time:"2020-08-19T13:04:55.166+00:00",
+        comments:[],
+        access:{
+          '5f3d23697086051e66f7a1b0': "Owner"
+        }
+      }
+    ]
   },
   mutations: {
     setFiles(state, files) {
