cf2014898c36b75473f3ef4ecd56a2e525f7596b
LiShushi <43929922+LiCarry@users.noreply.github.com>
2020-08-20 04:18:03 +0800

    File editor v1.5 (#95)
    * Wrong
    
    * FileEditorV1.5

--- Vue/src/network/edit.js
+++ Vue/src/network/edit.js
@@ -1,8 +1,12 @@
 import { request } from "@/network/request";
 
 export function newFile(userId, title, content) {
+  console.log("OK");
+  console.log(userId);
+  console.log(title);
+  console.log(content);
   return request({
-    url: '/user',
+    url: '/file/user',
     method: 'post',
     params: {
       userId: userId,
@@ -14,7 +18,7 @@ export function newFile(userId, title, content) {
 
 export function updateFile(userId, fileId, title, content) {
   return request({
-    url: '/user',
+    url: '/file/user',
     method: 'put',
     params: {
       userId: userId,
@@ -27,7 +31,7 @@ export function updateFile(userId, fileId, title, content) {
 
 export function getFile(userId, fileId) {
   return request({
-    url: '/user/all',
+    url: '/file/user/all',
     method: 'get',
     params: {
       userId: userId,
@@ -38,7 +42,7 @@ export function getFile(userId, fileId) {
 
 export function getFavorite(userId) {
   return request({
-    url: '/user/favorite',
+    url: '/file/user/favorite',
     method: 'get',
     params: {
       userId: userId
@@ -48,7 +52,7 @@ export function getFavorite(userId) {
 
 export function updateFavorite(userId, fileId) {
   return request({
-    url: '/user/favorite',
+    url: '/file/user/favorite',
     method: 'put',
     params: {
       userId: userId,
@@ -59,7 +63,7 @@ export function updateFavorite(userId, fileId) {
 
 export function recycleFile(fileId) {
   return request({
-    url: '/user/recycle',
+    url: '/file/user/recycle',
     method: 'put',
     params: {
       fileId: fileId
@@ -79,7 +83,7 @@ export function getRecycle(userId) {
 
 export function deleteFile(userId, fileId) {
   return request({
-    url: '/user',
+    url: '/file/user',
     method: 'delete',
     params: {
       userId: userId,
@@ -90,7 +94,7 @@ export function deleteFile(userId, fileId) {
 
 export function addComment(fileId, reviewerId, content) {
   return request({
-    url: '/comment/user',
+    url: '/comment/file',
     method: 'post',
     params: {
       fileId: fileId,
@@ -102,7 +106,7 @@ export function addComment(fileId, reviewerId, content) {
 
 export function deleteComment(fileId, commentId) {
   return request({
-    url: '/comment/user',
+    url: '/comment/file',
     method: 'delete',
     params: {
       fileId: fileId,
