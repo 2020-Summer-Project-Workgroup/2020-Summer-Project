fbf0d42ebdbcd39daa0ace6e4ed1c397a1bcb1cf
MelonLee-go <1376908007@qq.com>
2020-08-20 06:01:32 +0800

    new Collect

--- Vue/src/store/modules/files.js
+++ Vue/src/store/modules/files.js
@@ -2,7 +2,7 @@ export default {
   state: {
     files: [
       {
-        id: "5f3d23777086051e66f7a1b2",
+        id: "1",
         title: "LOL",
         content:"Hello World",
         time:"2020-08-19T13:04:55.166+00:00",
@@ -12,9 +12,19 @@ export default {
         }
       },
       {
-        id: "5f3d23777086051e66f7a784",
+        id: "2",
         title: "妈妈说学无止境，我不信，你信了吗？",
         content:"HelloADADAUSHFKDSBHAFGIUHGIUAHGIABHIAGHIUABFUIBASUIGFBAIFBUIWBFIUbfi",
+        time:"2020-08-19T13:04:55.166+00:00",
+        comments:[],
+        access:{
+          '5f3d23697086051e66f7a1b0': "Owner"
+        }
+      },
+      {
+        id: "3",
+        title: "LOL",
+        content:"Hello World",
         time:"2020-08-19T13:04:55.166+00:00",
         comments:[],
         access:{
