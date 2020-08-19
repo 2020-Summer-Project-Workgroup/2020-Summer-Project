e72712aa3c096a0fde3b152026fdb3fac063c0ec
LiShushi <43929922+LiCarry@users.noreply.github.com>
2020-08-20 05:56:53 +0800

    Update Comment component (#99)

--- Vue/src/components/Comment/Comment.vue
+++ Vue/src/components/Comment/Comment.vue
@@ -4,7 +4,7 @@
       <div id="top">
         <Edit></Edit>
       </div>
-      
+    
       <div id="middle">
         <span id="font_comment">
           评论
@@ -14,26 +14,29 @@
           <textarea name="reworkmes" cols="70" rows="3" id="TextareaComment" placeholder="请输入评论" v-model="commentInput"></textarea>
         </div>
         <div id="sendBut" @click="sendComment">
-          <a href="">发送</a>
+          <a>发送</a>
         </div>
       </div>
       <br />
     
       <div id="bottom">
         <div id="comments" v-for="comment in comments" :key="comment">
-          <i class="el-icon-s-custom" id="avatar"></i>
-          <div id="content">
+          <div id="oneComment" v-show="flag">
+            <i class="el-icon-s-custom" id="avatar"></i>
+            <div id="content">
             <div id="commentText">
               {{ comment.name }}
               : {{ comment.text }}
             </div>
             <div id="date">
-              <i class="el-icon-delete-solid" @click="delComment"></i>
+              <i class="el-icon-delete-solid" @click.prevent="delComment(comment.id)"></i>
               {{ comment.time }}
             </div>
             <br />
             <el-divider></el-divider>
           </div>
+          </div>
+          
         </div>
       </div>
     </div>  
@@ -52,28 +55,29 @@ export default {
   },
   data() {
     return {
+      flag: true,
       editorData: 'lss<div id="sendBut"><a href="">发送</a></div>',
       title: "",
+      num: 3,
       showDel: false,
-      commentInput: "",
-      comments: [{name: "李书实", text: "编辑器会自动进行上传，其需要服务器返回一个JSON数组，包含两个字段：uploaded(true / false)和url(相对路径)。举个例子如下。若没有返回内容或返回内容uploaded不为true，则会出现上传失败的情况。", time: "2020-08-19"}]
+      commentInput: "评论",
+      comments: [
+        {name: "李书实", text: "编辑器会自动进行上传，其需要服务器返回一个JSON数组，包含两个字段：uploaded(true / false)和url(相对路径)。举个例子如下。若没有返回内容或返回内容uploaded不为true，则会出现上传失败的情况。", time: "2020-08-19", id: 1},
+        {name: "吕俊洋", text: "一个线程访问数据库的时候，在成功获取到一个可用数据库连接之前的最长等待时间为2分钟这里比较危险，如果超过maxWaitTime都没有获取到这个连接的话，该线程就会抛出Exception故这里设置的maxWaitTime应该足够大，以免由于排队线程过多造成的数据库访问失败", time: "2020-08-20", id: 2},
+        {name: "韩子尧", text: "1.@RequestPart这个注解用在multipart/form-data表单提交请求的方法上。2.支持的请求方法的方式MultipartFile，属于Spring的MultipartResolver类。这个请求是通过http协议传输的。", time: "2020-08-11", id: 3}
+      ]
     }
   },
   methods: {
-    delComment: function() {
-      // let index = this.comments.indexOf(comment);
-      // if(index != -1){
-      //   this.comments.splice(index,1);
-      // }
-      // deleteComment(this.$store.getters.userId, fileId, this.commentInput);
+    delComment(index) {
+      this.comments.splice(index,1);
       console.log("Delete!");
     },
-    // sendComment: function(userId, commentInput){
-    //   
-    //   const time = new Date().toLocaleString();
-    //   this.comments.push({avatar: "", name: userId, text: this.commentInput, time: new Date().toLocaleString()});
-    //   addComment(fileId, userId, this.content);
-    // }
+    sendComment() {
+      // this.flag=true;
+      this.num++;
+      this.comments.push({name: "LSS", text: this.commentInput, time: new Date().toLocaleString(), id: this.num})
+    }
   }
 }
 </script>
@@ -95,7 +99,7 @@ a {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: center;
+  align-items: flex-start;
   position: absolute;
   /* top: 138px; */
   width: 50%;
@@ -104,12 +108,6 @@ a {
   /* padding-top: 4.61vh;
   padding-left: 4.53vw; */
   overflow-y: auto;
-  position: absolute;
-  left:17.6vw;
-  bottom: 0;
-  width: 62.5vw;
-  height: calc(100vh - 68px);
-  background-color: rgba(242, 244, 244, 1);
 }
 
 #top {
@@ -123,7 +121,7 @@ a {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  margin-top: 50rem;
+  margin-top: 48rem;
   width: 90%;
 }
 
@@ -157,13 +155,15 @@ a {
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  flex-shrink: 1;
+  flex-basis: 100%;
+  flex-shrink: 0;
 }
 
 #commentText {
   flex-shrink: 1;
   color: #616161;
   font-family: "Microsoft YaHei", "微软雅黑";
+  width: 100%;
   font-size: 20px;
 }
 
